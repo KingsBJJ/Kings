11bf68f8067475ec0f3ead5a808726bb28bcb1eb
KingsBJJ <kings.bjjoficial@gmail.com>
2025-06-15 13:22:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+/ app.js
+import { initializeApp } from "firebase/app";
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCTujv7x2GVfRJ9j_9oKPtslxTopCaa4vI",
+  authDomain: "bjj-academy-manager.firebaseapp.com",
+  projectId: "bjj-academy-manager",
+  storageBucket: "bjj-academy-manager.firebasestorage.app",
+  messagingSenderId: "277792852037",
+  appId: "1:277792852037:web:82e448d0f7022201ecbb2c"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
